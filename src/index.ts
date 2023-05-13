3c54fd978cfdcc382f3ebe93a4fe7e52d7e46747
John Eletto <johnanthony.eletto@gmail.com>
2023-05-12 20:58:15 -0400

    Add thermostat accessory

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { API } from 'homebridge';
 
 import { PLATFORM_NAME } from './settings';
-import { ExampleHomebridgePlatform } from './platform';
+import { WindmillThermostatAccessory } from './accessory';
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  api.registerAccessory(PLATFORM_NAME, WindmillThermostatAccessory);
 };
